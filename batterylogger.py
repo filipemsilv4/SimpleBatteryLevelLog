1b3d3ab7ef1a172604a3c3ba884a08270bd0c740
Paulo Filipe Moreira da Silva <59426206+filipemsilv4@users.noreply.github.com>
2023-09-21 16:22:14 -0300

    Some improvements in the output

--- batterylogger.py
+++ batterylogger.py
@@ -2,6 +2,14 @@ import time
 import psutil
 import locale
 
+def calculate_elapsed_time(start_time):
+    # Calculates the elapsed time between the start and the current time.
+    current_time = time.time()
+    elapsed_seconds = current_time - start_time
+    hours, remainder = divmod(elapsed_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{int(hours)}:{int(minutes)}:{int(seconds)}"
+
 # Detects the operating system language
 system_language, _ = locale.getdefaultlocale()
 
@@ -21,7 +29,7 @@ strings = {
     },
 }
 
-# Verifies if the system language is available, otherwise, uses english as default
+# Verifies if the system language is available, otherwise, uses English as default
 if system_language in strings:
     selected_strings = strings[system_language]
 else:
@@ -29,16 +37,21 @@ else:
 
 log_file = selected_strings['file_name']
 
+# Get the start time
+start_time = time.time()
+
 with open(log_file, "w") as file:
-    file.write(selected_strings['start_log'].format(time.strftime("%Y-%m-%d %H:%M:%S")))
+    file.write("[ " + selected_strings['start_log'].format(time.strftime("%Y-%m-%d %H:%M:%S")) + " ]\n")
 
 while True:
     current_time = time.strftime("%Y-%m-%d %H:%M:%S")
     battery_percent = psutil.sensors_battery().percent
 
+    elapsed_time = calculate_elapsed_time(start_time)
+
     with open(log_file, "a") as file:
-        file.write(selected_strings['elapsed_time'].format(current_time))
-        file.write(selected_strings['battery_level'].format(battery_percent))
+        file.write("[ " + selected_strings['elapsed_time'].format(elapsed_time) + " ] ")
+        file.write("[ " + selected_strings['battery_level'].format(battery_percent) + " ]")
         file.write("\n")
 
     time.sleep(60)  # Waits 60 seconds
